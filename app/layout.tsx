5880e70499c48d13216d749e970efbdafdff497e
surawich-s <surawich.sit@gmail.com>
2024-08-30 22:45:20 +0700

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import './globals.css'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>Prompt Manager</title>
+        <meta name="description" content="A simple prompt management application" />
+      </head>
       <body>{children}</body>
     </html>
   )
